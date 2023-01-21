faa946df384d6effe2bffd2041d5058f9ace66f7
vkalidas <vkalidas@localhost>
2023-01-20 17:36:51 -0700

    added new java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,18 @@
 class HelloWorld {
     public static void main(String[] args) {
-        int i = 2;
+        int i = 10;
+	int ret = 0;
 	System.err.println("Hello world: start workload");
-	while (i > 0) { // Mchine code is not generaes
-            workload(14, 2);
+	while (--i > 0) { // Mchine code is not generaes
+            ret += workload(14, i);
         }
+	System.err.println(ret);
     }
 
     private static int workload(int a, int b) {	
 	//System.out.println("Hello world: from compiled workload");
-        return a - b;
+	int x = a - b;
+	int z = x * 10;
+        return z;
     }
 }
